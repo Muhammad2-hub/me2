cd9531abf2ba12803cedbec1b00e101171f782e1
muhammad <issahmuhammad2@gmail.com>
2020-07-24 02:38:20 -0700

    big

---
+++ test.py
@@ -0,0 +1,8 @@
+name = "Muhammad"
+
+is_raining = True
+
+rating = 12.5
+
+sentence = """
+ i lve in Yendi in the northern region of Ghana, it is known to be the greatest land in the Dagbon kingdom since it houses the paramount chief of the Kingdom"""

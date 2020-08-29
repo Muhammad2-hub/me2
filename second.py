7face2480457ec6a441a8beb4e7ac16081b4871b
= <=>
2020-08-29 12:14:53 -0700

    practice

---
+++ second.py
@@ -0,0 +1,13 @@
+name = "muhammad"
+last = "issah"
+
+full = name + " "+ last
+
+print(full)
+
+greetings = "good morning"
+being = "hope you slept well"
+
+full_greetings= f"{greetings} {being}"
+
+print(full_greetings)
---
+++ strip.py
@@ -0,0 +1,5 @@
+name = "issah"
+last = "muhammad"
+full_name = f"{name} {last}"
+capitalized =full_name.upper()
+print(capitalized)
